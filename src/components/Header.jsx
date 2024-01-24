d61e6fc84c9b6f31a4eeef1216259b322848d96e
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-01-24 08:23:21 +0100

    phone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,13 +17,13 @@ const Header = () => {
 					type="text"
 					placeholder="Recherche des articles"
 				/>
-				<button className="text-blue-vinted border-blue-vinted border rounded-md p-2 m-2 w-40 text-xs max-md:w-1/4">
+				<button className="text-blue-vinted border-blue-vinted border rounded-md p-2 m-2 w-40 text-xs max-md:w-1/3">
 					S'inscrire
 				</button>
-				<button className="text-blue-vinted  border-blue-vinted border rounded-md p-2 m-2 w-40 text-xs max-md:w-1/4">
+				<button className="text-blue-vinted  border-blue-vinted border rounded-md p-2 m-2 w-40 text-xs max-md:w-1/3">
 					Se connecter
 				</button>
-				<button className="bg-blue-vinted  rounded-md p-2 m-2 text-white w-40 text-xs max-md:w-1/4">
+				<button className="bg-blue-vinted  rounded-md p-2 m-2 text-white w-40 text-xs max-md:w-1/3">
 					Vends tes articles
 				</button>
 			</header>
